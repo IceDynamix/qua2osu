160e959c5383e97f023e32268da5c6c5613fcd45
IceDynamix <aerozocker@gmail.com>
2019-12-18 22:58:17 +0100

    Update gui paths

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gui.ui'
+# Form implementation generated from reading ui file 'gui/gui.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.2
 #
